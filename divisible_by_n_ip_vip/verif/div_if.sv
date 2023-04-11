95e78a591dafb9cb875367fc935c3870d2e0e5be
Will Chen <will@chendot.com>
2023-04-10 21:03:23 -0700

    Assertions to enforce result valid timing and register read XOR write
    * Added assertions to following interfaces that enforce expected
      properties and timing of interface signals:
       * div_if (divisibility checker interface):
          * Result must not remain valid for more than 1 clock following
            reset
          * Result must be valid exactly 1 clock following bitstream valid
          * Result must be invalid exactly 1 clock following bitstream
            invalid
       * reg_if (register bus interface):
          * Concurrent read and write requests must never occur

--- divisible_by_n_ip_vip/verif/div_if.sv
+++ divisible_by_n_ip_vip/verif/div_if.sv
@@ -2,7 +2,9 @@
 // Author: Will Chen
 //
 // Description:
-//    * Interface between divisibility checker module and testbench
+//    * Interface signals between divisibility checker module and testbench
+//    * Assertions that enforce expected properties and timing of interface
+//      signals
 ///////////////////////////////////////////////////////////////////////////////
 
 
@@ -17,5 +19,16 @@ interface div_if();
     // Outputs of divisibility checker module
     logic divisible;
     logic result_vld;
+
+    // Assertions
+    result_vld_despite_rst:
+        assert property (@(posedge clk) !rst_n |-> ##[1:2] !result_vld)
+        else `uvm_error("IF", "Result remained valid for more than 1 clock following reset!");
+    result_vld_delay:
+        assert property (@(posedge clk) bitstream_vld |-> ##1 result_vld)
+        else `uvm_error("IF", "Result not valid exactly 1 clock following bitstream valid!");
+    result_invld_delay:
+        assert property (@(posedge clk) !bitstream_vld |-> ##1 !result_vld)
+        else `uvm_error("IF", "Result not invalid exactly 1 clock following bitstream invalid!");
 endinterface : div_if
 
--- divisible_by_n_ip_vip/verif/reg_if.sv
+++ divisible_by_n_ip_vip/verif/reg_if.sv
@@ -2,7 +2,8 @@
 // Author: Will Chen
 //
 // Description:
-//    * Interface between register bus and testbench
+//    * Interface signals between register bus and testbench
+//    * Assertions that enforce expected properties of interface signals
 ///////////////////////////////////////////////////////////////////////////////
 
 
@@ -15,5 +16,12 @@ interface reg_if();
     logic [`REG_ADDR_SZ-1:0] reg_addr;
     logic [`REG_DATA_SZ-1:0] reg_wr_data;
     logic [`REG_DATA_SZ-1:0] reg_rd_data;
+
+    // Assertions
+    concurrent_rd_wr:
+        assert property (@(posedge clk) !(reg_rd_en && reg_wr_en))
+        else `uvm_error("REG_IF",
+                        $sformatf("Register bus concurrent read and write of address 0x%0h!",
+                                  reg_addr));
 endinterface : reg_if
 
